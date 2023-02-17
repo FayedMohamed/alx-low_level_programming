d91fccc29b651b8ba50ac1f52c90ceb4434cc76a
fayed <fayed@gmail.com>
2023-02-16 16:16:12 -0800

    without printf or puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+ * main - Entry value
+ * Description: 'A program to print without printf or puts'
+ * Return: Always 1 (success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+
+
+
+
+}
