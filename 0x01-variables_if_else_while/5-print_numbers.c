f9bfa4672f2b62500072666bf6742eb159ea4d18
fayed <fayed@gmail.com>
2023-02-17 17:54:29 -0800

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stido.h>
+/**
+ * main - Entry value
+ * Description: 'A program to print all base 10 numbers'
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
