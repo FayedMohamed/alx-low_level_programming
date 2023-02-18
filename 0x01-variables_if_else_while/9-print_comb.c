ee9ffbe09ab3e00ccfff9aa94f406cb5c6357f33
fayed <fayed@gmail.com>
2023-02-17 18:16:45 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main - Entry value
+ * Description: 'A program to print all possible combination'
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
